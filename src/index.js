162449f1e466aebc4a8b3e6ee9a8a722e219d258
Marc <marcsanchez@me.com>
2022-04-03 12:59:17 -0400

    Competed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTodo(e.target.newTodo.value)
+  })
+
+function createTodo(todo) {
+  const li = document.createElement('li')
+  const btn = document.createElement('button')
+  
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'X'
+  li.textContent = `${todo} `
+  li.append(btn)
+  console.log(li)
+  document.querySelector('#tasks').append(li)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
+
